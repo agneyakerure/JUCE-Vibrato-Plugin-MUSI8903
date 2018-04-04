d74c20f89aef870e8afbda1f2752b4b90d63deec
agneyakerure <kerure.agneya@gmail.com>
2018-04-03 23:02:25 -0400

    merge conflicts resolved

--- VibratoPlugin/Source/PluginEditor.cpp
+++ VibratoPlugin/Source/PluginEditor.cpp
@@ -35,19 +35,9 @@ VibratoPluginAudioProcessorEditor::VibratoPluginAudioProcessorEditor (VibratoPlu
     widthSlider.addListener(this);
     addAndMakeVisible(frequencySlider);
     
-    widthSlider.setSliderStyle(Slider::SliderStyle::Rotary);
-    widthSlider.setValue(0.001);
-    widthSlider.setTextBoxStyle(Slider::TextBoxBelow, true, 100, 25);
-    widthSlider.setRange(0.001, 0.02);
-    widthSlider.addListener(this);
-    addAndMakeVisible(widthSlider);
-    
     setSize (600, 110);
     addAndMakeVisible (bypassButton);
     bypassButton.setButtonText ("Bypass");
-    
-    
-    
 }
 
 VibratoPluginAudioProcessorEditor::~VibratoPluginAudioProcessorEditor()
@@ -66,42 +56,29 @@ void VibratoPluginAudioProcessorEditor::paint (Graphics& g)
 }
 
 void VibratoPluginAudioProcessorEditor::resized()
-<<<<<<< HEAD
 {
-    // This is generally where you'll want to lay out the positions of any
-    // subcomponents in your editor..
-    frequencySlider.setBounds(getLocalBounds());
-    widthSlider.setBounds(getLocalBounds());
-=======
-{
-    // This is generally where you'll want to lay out the positions of any
-    // subcomponents in your editor..
     const int border = 20;
     const int dialWidth = getWidth()/2 - border;
     const int dialHeight = getHeight()/2 - border;
     
     frequencySlider.setBounds(border, border, dialWidth, dialHeight);
     widthSlider.setBounds((getWidth() / 2), border, dialWidth, dialHeight);
->>>>>>> d07aba527b831d67450f531526b00354d7bda539
 }
 
-void buttonClicked (Button* button)
+void VibratoPluginAudioProcessorEditor::buttonClicked(Button* button)
 {
     
 }
 
-void VibratoPluginAudioProcessorEditor::sliderValueChanged(Slider *slider){
+void VibratoPluginAudioProcessorEditor::sliderValueChanged(Slider *slider)
+{
     if(slider == &frequencySlider)
     {
-        //DBG(std::to_string(pCVibrato->getParam(CVibrato::kParamModFreqInHz)));
-        //DBG(std::to_string(frequencySlider.getValue()));
         processor.setVibratoParameter(CVibrato::kParamModFreqInHz, (float)slider->getValue());
     }
     
     if(slider == &widthSlider)
     {
-        //DBG(std::to_string(pCVibrato->getParam(CVibrato::kParamModFreqInHz)));
-        //DBG(std::to_string(frequencySlider.getValue()));
         processor.setVibratoParameter(CVibrato::kParamModWidthInS, (float)slider->getValue());
     }
     
